e3cfc582b532a88849fcb9a32d581c16b1e4e4c2
Antonio Enríquez <antonio.enralv@gmail.com>
2021-05-23 12:40:02 -0500

    promesa

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-//Callbacks ejemplos
-function primera(call1, call2){
-    setTimeout(() =>{
-        console.log("1");
-        call1();
-        call2();
-    }), 3000;
-}
-function segunda(){
-    console.log("2");
-}
-function tercera(){
-    console.log("3");
-}
+//Promesas
+let promesa = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		let numero = Math.random();
+		console.log(numero);
+		if(numero >= 0.5) resolve("Exito");
+		reject("Error");
+	}, 500);
+})
 
-primera(segunda, tercera);
+promesa
+    .then((data)=> console.log("Then ", data))
+    .catch((error)=> console.log("Catch ", error))
